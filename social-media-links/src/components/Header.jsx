f415e43b63b080d40645905d2311ab4f7cc0ce81
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2022-10-02 11:43:25 +0300

    better mobile view

--- social-media-links/src/components/Header.jsx
+++ social-media-links/src/components/Header.jsx
@@ -4,7 +4,7 @@ import myImage from "../assets/mypic.jpg";
 const Header = () => {
   return (
     <div className="container mx-auto px-4">
-      <div className="flex flex-col gap-8 py-16 justify-center items-center w-full">
+      <div className="flex flex-col gap-6 py-4 justify-center items-center w-full md:py-16 md:gap-8">
         <img src={myImage} alt="myImage"  className="w-32 rounded-full md:w-48"/>
         <h1 className="text-gray-100 font-bold drop-shadow-lg font-robotoMono text-4xl">Eetu Rönkkö</h1>
       </div>
